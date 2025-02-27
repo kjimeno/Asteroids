3dd5cbad7997710a96d585bb5fe045e55099f816
Ken J. <kjimeno2@academic.rrc.ca>
2025-02-27 11:03:16 -0600

    Ship: Added Rotation Mechanic and Input Processing

--- ship.js
+++ ship.js
@@ -1,21 +1,39 @@
 class Ship {
-  constructor(x, y, size) {
+  constructor(x, y, size, rotation, rotationSpeed) {
     this.x = x;
     this.y = y;
     this.size = size;
+    this.rotation = rotation;
+    this.rotationSpeed = rotationSpeed;
+  }
+
+  processInput() {
+    //'A' is pressed
+    if (keyIsDown(65)) {
+      this.rotation -= this.rotationSpeed;
+    }
+
+    //'D' is pressed
+    if (keyIsDown(68)) {
+      this.rotation += this.rotationSpeed;
+    }
   }
 
   display() {
     push();
+
+    translate(this.x, this.y);
+    rotate(this.rotation);
+
     triangle(
-      this.x - this.size / 2,
-      this.y + this.size / 2,
-      this.x - this.size / 2,
-      this.y - this.size / 2,
-      this.x + this.size / 2,
-      this.y
+      -this.size / 2,
+      this.size / 2,
+      -this.size / 2,
+      -this.size / 2,
+      this.size / 2,
+      0
     );
+
     pop();
-    console.log(this.x);
   }
 }
--- sketch.js
+++ sketch.js
@@ -2,9 +2,11 @@ let ship;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  ship = new Ship(width / 2, height / 2, 20);
+  ship = new Ship(width / 2, height / 2, 20, -PI / 2, 0.1);
 }
 
 function draw() {
+  background(255);
+  ship.processInput();
   ship.display();
 }
