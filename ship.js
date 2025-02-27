8bb275ec7bba7d575e815d8d18de04d81eb42221
Ken J. <kjimeno2@academic.rrc.ca>
2025-02-27 12:13:37 -0600

    Added a Teleport Mechanic
    Player can now press the 's' key to teleport to a random location within the canvas.

--- ship.js
+++ ship.js
@@ -2,11 +2,15 @@ class Ship {
   constructor(position, size, rotation, rotationSpeed, thrustPower, dragForce) {
     this.position = position;
     this.size = size;
+    this.initSize = size;
     this.rotation = rotation;
     this.rotationSpeed = rotationSpeed;
     this.velocity = createVector(0, 0);
     this.thrustPower = thrustPower;
     this.dragForce = dragForce;
+    this.teleportActive = false;
+    this.teleportShrinking = false;
+    this.teleportAnimSpeed = 0.8;
   }
 
   processInput() {
@@ -29,13 +33,44 @@ class Ship {
         )
       );
     }
+
+    if (keyIsDown(83) && !this.teleportActive) {
+      this.teleportActive = true;
+      this.teleportShrinking = true;
+    }
   }
 
   update() {
-    console.log(this.velocity);
+    //Update the velocity and position based on the drag force
     this.velocity.mult(this.dragForce);
-
     this.position.add(this.velocity);
+
+    //If the ship is teleporting, currently shrinking, and is not small enough ==> keep shrinking its size
+    if (this.teleportActive && this.teleportShrinking && this.size >= 0.1) {
+      this.size *= this.teleportAnimSpeed;
+    }
+    //If the ship is teleporting, currently shrinking, BUT is SMALL enough ==> teleport to a random location
+    else if (this.teleportActive && this.teleportShrinking && this.size < 0.1) {
+      this.teleportShrinking = false;
+      this.position = createVector(random(windowWidth), random(windowHeight));
+    }
+    //If the ship is teleporting, NO LONGER shrinking, and is NOT as big as its original size ==> keep growing its size
+    else if (
+      this.teleportActive &&
+      !this.teleportShrinking &&
+      this.size < this.initSize
+    ) {
+      this.size *= 2 - this.teleportAnimSpeed;
+    }
+    //If the ship is teleporting, not shrinking, and is back to the original size ==> set 'teleportActive' to false
+    else if (
+      this.teleportActive &&
+      !this.teleportShrinking &&
+      this.size >= this.initSize
+    ) {
+      this.size = this.initSize;
+      this.teleportActive = false;
+    }
   }
 
   display() {
