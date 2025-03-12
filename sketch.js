0dab78cc4411b0fb8ccdf9fa51aa98720a2f3c73
Ken J. <kbjimeno@gmail.com>
2025-03-11 21:55:25 -0500

    Music Plays in the Background

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
 let gameManager;
 let font;
+let music;
 
 function preload() {
   font = loadFont("assets/fonts/pixelated.ttf");
+  music = createAudio("assets/sfx/music.mp3");
 }
 
 function setup() {
   createCanvas(windowWidth * 0.99, windowHeight * 0.99);
   textAlign(CENTER);
   textFont(font);
+
+  //Loop the music
+  music.loop();
 
   gameManager = new GameManager(4, 0);
   gameManager.startGame();
